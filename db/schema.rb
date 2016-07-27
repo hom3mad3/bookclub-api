7870ab8fe24743406b46da4a714a1f149c610779
hom3mad3 <micaelarava@gmail.com>
2016-07-27 17:14:04 +0200

    created authors and migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160715131226) do
+ActiveRecord::Schema.define(version: 20160727150808) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
+    t.string   "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
