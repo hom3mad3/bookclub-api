2b824dc2770744be91c38a590b4a18673fdd4e43
hom3mad3 <micaelarava@gmail.com>
2016-07-28 10:49:20 +0200

    migrate db models update author | book | user

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160727150808) do
+ActiveRecord::Schema.define(version: 20160727152050) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
     t.string   "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.integer  "author_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
